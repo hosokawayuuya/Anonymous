eef859d08408138f1a4cea4f3c60886c7e75be17
sota20040308 <2201124@s.asojuku.ac.jp>
2024-05-29 10:56:53 +0900

    こみ

--- reset2.php
+++ reset2.php
@@ -5,10 +5,18 @@ try {
     // データベースに接続する
     $con = new PDO($connect, USER, PASS);
  
-    // ログを削除するSQLクエリを作成
+
     $delete_sql = "DELETE FROM Log";
     $delete_stmt = $con->prepare($delete_sql);
     $delete_stmt->execute();
+
+    $delete_room_sql = "DELETE FROM Room";
+    $delete_room_stmt = $con->prepare($delete_room_sql);
+    $delete_room_stmt->execute();
+
+    
+
+
  
     echo "すべてのログが正常に削除されました。";
  
