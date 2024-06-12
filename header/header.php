1b5016023d5ef22eb908256d545d99017a20a3bc
sota20040308 <2201124@s.asojuku.ac.jp>
2024-06-12 10:13:12 +0900

    こみ

--- header/header.php
+++ header/header.php
@@ -4,6 +4,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Anonymous</title>
-    <h1>参加人数: <span id="userCount"><?php echo $userCount; ?></span></h1>
 </head>
+<header>
+        <h1>参加人数: <span id="userCount"><?php echo $userCount; ?></span></h1>
+        <nav>
+            <ul class="nav-buttons">
+                <li><button onclick="location.href='kopyURL.php'">URL</button></li>
+                <li><button onclick="location.href='rule.php'">ルール</button></li>
+                <li><button onclick="location.href='reset2.php'">リセット</button></li>
+            </ul>
+        </nav>
+    </header>
 <body>
