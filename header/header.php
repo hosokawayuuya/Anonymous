475d107b34dfd5c1227f6a7c6ea5d1e1d258bded
sota20040308 <2201124@s.asojuku.ac.jp>
2024-06-12 10:57:38 +0900

    komi

--- header/header.php
+++ header/header.php
@@ -10,9 +10,9 @@
         <h1>参加人数: <span id="userCount"><?php echo $userCount; ?></span></h1>
         <nav>
             <ul class="nav-buttons">
-                <li><button onclick="location.href='kopyURL.php'">URL</button></li>
-                <li><button onclick="location.href='rule.php'">ルール</button></li>
-                <li><button onclick="location.href='reset2.php'">リセット</button></li>
+                <button onclick="location.href='kopyURL.php'">URL</button>
+                <button onclick="location.href='rule.php'">ルール</button>
+                <button onclick="location.href='reset2.php'">リセット</button>
             </ul>
         </nav>
     </header>
